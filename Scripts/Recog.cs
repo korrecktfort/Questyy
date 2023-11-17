02978f5b2e66845c6ea85fc1f839f2ecb8a4189c
LAPTOP-MHRHNM04\Bernhard <klemens.reckfort@gmx.de>
2023-11-17 20:59:30 +0100

    voice recognition setup

---
+++ Scripts/Recog.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System;
+using System.Linq;
+
+/// <summary>
+/// Using UnityEngine.Windows.Speech to recognize speech
+/// </summary>
+public class Recog : MonoBehaviour
+{
+    KeywordRecognizer keywordRecognizer = null;
+    Dictionary<string, Action> dictionary = new Dictionary<string, Action>();
+
+    [SerializeField] private KeywordAction[] keywordActions = default;
+    
+    private void Start()
+    {
+        foreach(KeywordAction keywordAction in keywordActions)
+            dictionary.Add(keywordAction.keyword, keywordAction.distribution.Action.Invoke);
+        
+
+        keywordRecognizer = new KeywordRecognizer(dictionary.Keys.ToArray());
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech;
+
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs args)
+    {
+        Action action;
+        if(dictionary.TryGetValue(args.text, out action))
+        {
+            action.Invoke();
+        }
+    }
+}   
---
+++ Scripts/RecogDistribution.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RecogDistribution : MonoBehaviour
+{
+    public Action Action => action;
+
+    protected Action action = default;
+
+
+}
